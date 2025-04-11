5815c03abab21d5ca3e66bcf88224ff168552e93
Vlad Pirusca <vpiru077@uottawa.ca>
2025-04-11 17:01:26 -0400

    Initial React app setup for Gee-Gees Tennis Club

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Gee-Gees Tennis Club</h1>
+      <p>Welcome to the official site of our tennis club/team/wtv.</p>
+    </div>
+  );
+}
+
+export default App;
