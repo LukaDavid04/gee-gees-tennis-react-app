809056f9f2c4d214ca2147dbd0e3f5cdcee60681
Vlad Pirusca <vpiru077@uottawa.ca>
2025-05-17 11:03:33 -0400

    these are all the complete changes to the HOME page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,21 +5,36 @@ function Footer() {
   return (
     <footer className="text-center text-white" style={{ backgroundColor: '#ebe9e9' }}>
       <div className="container pt-3">
-        <h6 style={{ color: '#4E0B12' }}>Connect with us!</h6>
-        <section className="mb-3">
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1 social-media-icon" href="https://www.instagram.com/geegeestennis/">
+        <h6 className="connect-heading">Connect with us!</h6>
+        <section className="social-icons">
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1 social-media-icon"
+            href="https://www.instagram.com/geegeestennis/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-instagram"></i>
           </a>
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1 social-media-icon" href="https://www.tiktok.com/@geegeestennis">
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1 social-media-icon"
+            href="https://www.tiktok.com/@geegeestennis"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-tiktok"></i>
           </a>
-          <a className="btn btn-link btn-floating btn-lg text-dark m-1 social-media-icon" href="mailto:universityofottawatennis@gmail.com">
+          <a
+            className="btn btn-link btn-floating btn-lg text-dark m-1 social-media-icon"
+            href="mailto:universityofottawatennis@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fas fa-envelope"></i>
           </a>
         </section>
       </div>
-      <div className="d-flex align-items-center justify-content-center p-3" style={{ backgroundColor: '#4E0B12' }}>
-        <img className="mr-2" src="images/geegees_logo_white.png" width="35" height="30" alt="" />
+      <div className="footer-bottom">
+        <img src="images/geegees_logo_white.png" width="40" height="35" alt="" />
         University of Ottawa Tennis Club
       </div>
     </footer>
